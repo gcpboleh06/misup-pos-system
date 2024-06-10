4ff884c039ba85c7ceb97465c86d78e78a96a4e4
Aizat <subgrenadier@gmail.com>
2024-06-11 01:15:23 +0800

    add vercel

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import '.././App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -171,6 +172,7 @@ function App() {
                     </div>
                 </div>
             </div>
+            <SpeedInsights />
         </div>
     );
 }
